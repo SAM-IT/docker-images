0f0a3664576718a77ca597c0c682e36ec495ece6
Sam Mousa <sam@mousa.nl>
2016-12-27 09:31:34 +0100

    Added missing file

--- rancher-nginx/start.sh
+++ rancher-nginx/start.sh
@@ -2,10 +2,9 @@
 
 #Download stuff.
 curl -L "https://$GITLAB_URL/api/v3/projects/$PROJECT_ID/builds/artifacts/$REFERENCE/download?job=$JOB" \
-  --header "PRIVATE-TOKEN: $ACCESS_TOKEN"
+  --header "PRIVATE-TOKEN: $ACCESS_TOKEN" \
   -o /artifacts.zip
-unzip /artifacts.zip -d /www
+unzip /artifacts.zip -d /tmp
+mv /tmp/dist/* /www/
+rm -rf /tmp
 rm /artifacts.zip
-
-# Start nginx.
-exec nginx -g daemon off;
